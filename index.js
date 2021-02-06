596b28afbc2ab9d1ff93e6cac0e0a88701c3d717
unknown <jeffreyg2240@gmail.com>
2021-02-05 21:13:48 -0500

    fixed a bunch of naming errors in .get

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const pool = new Pool({
 });
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
-app.get('/  ', (req, res) => res.sendFile(`${__dirname}/sortedName.html`));
+app.get('/sortedClassName', (req, res) => res.sendFile(`${__dirname}/sortedName.html`));
 app.get('/classInfo', async (req, res) => {
     const info = await readInfo();
     res.setHeader('content-type','application/json');
