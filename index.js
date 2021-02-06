524a0a49699de2c088cd7962de94fcaf2f5c7898
unknown <jeffreyg2240@gmail.com>
2021-02-06 17:50:44 -0500

    Added all the comments to my project

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const http = require('http');
 const express = require("express");
 const app = express();
 app.use(express.json());
+// process.env.PORT is given by heroku
 const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
+// credientals for Heroku
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -16,15 +18,20 @@ const pool = new Pool({
     // "password": 'postgre123',
     // "database": 'Schedule'
 });
+// Tells you that the server is up
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
+// index.html for main directory using express
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+// Sorted class names file
 app.get('/sortedClassName', (req, res) => res.sendFile(`${__dirname}/sortedName.html`));
 app.get('/classInfo', async (req, res) => {
+    // awaits for the file to read the JSON data from the get request before contiuning the promise
     const info = await readInfo();
     res.setHeader('content-type','application/json');
     res.send(JSON.stringify(info));
 });
 app.post('/classInfo', async (req, res) => {
+    // creates the info to push onto the database
     let result = {};
     try{
         const reqJson = req.body;
@@ -39,6 +46,7 @@ app.post('/classInfo', async (req, res) => {
     }
 });
 app.delete("/classInfo", async (req, res) => {
+    // deletes the info off the database
     let result = {}
     try{
         const reqJson = req.body;
@@ -51,7 +59,8 @@ app.delete("/classInfo", async (req, res) => {
         res.setHeader('content-type','application/json');
         res.send(JSON.stringify(result));
     }
-})
+});
+// same thing as read but for the sorted page
 app.get("/sortedInfo", async (req, res) => {
     let result = {}
     try{
@@ -65,8 +74,39 @@ app.get("/sortedInfo", async (req, res) => {
         res.setHeader('content-type','application/json');
         res.send(JSON.stringify(result));
     }
-})
+});
+app.post('/sortedInfo', async (req, res) => {
+    let result = {};
+    try{
+        const reqJson = req.body;
+        await createInfo(reqJson.classInfo);
+        result.sucess = true;
+    }catch(e){
+        result.sucess = false;
+        console.log(`Error ${e}`);
+    }finally{
+        res.setHeader('content-type','application/json');
+        res.send(JSON.stringify(result));
+    }
+});
+app.delete("/sortedInfo", async (req, res) => {
+    let result = {}
+    try{
+        const reqJson = req.body;
+        result.success = await deleteInfo(reqJson.id)
+    }
+    catch(e){
+        result.success=false;
+    }
+    finally{
+        res.setHeader('content-type','application/json');
+        res.send(JSON.stringify(result));
+    }
+});
+
+// connect();
 start();
+// more conventional when needed to connect to multiple databases and use promises in the start function
 async function start(){
     await connect();
 }
@@ -77,6 +117,7 @@ async function connect(){
         console.log(`Error ${e}`);
     }
 }
+// query for read
 async function readInfo(){
     try{
         const res = await pool.query('SELECT * FROM classlist;');
@@ -85,6 +126,7 @@ async function readInfo(){
         return[];
     }
 }
+// query for read sorted
 async function sortInfo(){
     try{
         const res = await pool.query('SELECT * FROM classlist ORDER BY course_name');
@@ -93,6 +135,7 @@ async function sortInfo(){
         return[];
     }
 }
+// query for post
 async function createInfo(info){
     try{
         console.log(info);
@@ -103,6 +146,7 @@ async function createInfo(info){
         return false;
     }
 }
+// query for delete
 async function deleteInfo(id){
     try {
         await pool.query("DELETE FROM classlist WHERE id = $1", [id]);
